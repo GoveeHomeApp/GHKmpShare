ef1473ba3a100ec8da069454ac1f2e9c9f3c9072
songyang <yang.song@govee.com>
2025-07-11 10:49:52 +0800

    [Modify] 补充kmp相关接口

--- GHKmpShare/Classes/Biz/KMPSharedBiz.swift
+++ GHKmpShare/Classes/Biz/KMPSharedBiz.swift
@@ -7,28 +7,141 @@
 
 import Foundation
 import Shared
+import HandyJSON
 
 @objcMembers
 public class KMPSharedBiz: NSObject {
+    /** KMP业务中间层
+     * 只接受dict类型传入
+     *
+     */
+    public private(set) static var shared = KMPSharedBiz()
     
-    public private(set) var shared = KMPSharedBiz()
+    private var currentScene: (dto: KmpBizDto, device: KmpDeviceDto)? = nil
     
-    /// 解析场景速度UI
-    public func speedUIParam(sceneBizParam: [String: Any], device: [String: Any]) -> KmpSpeedVo? {
-        
-        
+    private var currentKmpProtocol: IKmpProtocol? = nil
+    
+    /// 当前KMP功能是否支持此SKU的此场景 -
+    public func isSupportSceneBiz(param: [String: Any]?, device: [String: Any]?) -> Bool {
+        if let dto = KmpBizDto.deserialize(from: param), let dv = KmpDeviceDto.deserialize(from: device) {
+            let bytes = Data(base64String: dto.base64String) ?? Data()
+            var extString = ""
+            if let dict = dv.deviceExt, let jsonData = try?JSONSerialization.data(withJSONObject: dict, options: []) {
+                extString = String(data: jsonData, encoding: .utf8) ?? ""
+            }
+            let deviceInfo = KmpDeviceInfo(sku: dv.sku, name: dv.deviceName, device: dv.deviceID, goodsType: Int32(dv.goodsType), softVersion: dv.versionSoft, hardVersion: dv.versionHard, pactType: Int32(dv.pactType), pactCode: Int32(dv.pactCode), ext: extString)
+            // 当前就是这个场景 return
+//            if let current = self.currentScene, let impl = currentKmpProtocol, current.dto.scenesType == dto.scenesType && current.device.deviceID == dv.deviceID && current.device.sku == dv.sku {
+//                return true
+//            }
+            if let pt = KmpProtocolHelper.shared.parse(sceneType: Int32(dto.scenesType), effectBytes: bytes.toKotlinByteArray(), info: deviceInfo) {
+                self.currentKmpProtocol = pt
+                self.currentScene = (dto, dv)
+                return true
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
+    }
+    
+    public func getCurrentBytes(param: [String: Any]?, device: [String: Any]?) -> String? {
+        var res: String? = nil
+        if self.isSupportSceneBiz(param: param, device: device) {
+            res = _getCurrentBytes()
+        }
+        return res
+    }
+    
+    private func _getCurrentBytes() -> String? {
+        var res: String? = nil
+        if let pt = self.currentKmpProtocol, let datas = pt.bytes() {
+            let trans = datas.toData()
+            res = trans.base64EncodedString()
+        }
+        return res
+    }
+    
+    public func changeColor(h: Float, param: [String: Any]?, device: [String: Any]?) -> String? {
+        var res: String? = nil
+        if self.isSupportSceneBiz(param: param, device: device), let pt = self.currentKmpProtocol {
+            pt.updateColor(h: h)
+            res = _getCurrentBytes()
+        }
+        return res
+    }
+    
+    public func resetColor(param: [String: Any]?, device: [String: Any]?) -> String? {
+        var res: String? = nil
+        if self.isSupportSceneBiz(param: param, device: device), let pt = self.currentKmpProtocol {
+            pt.resetColor()
+            res = _getCurrentBytes()
+        }
+        return res
+    }
+    
+    /// 调整方向 (注意：方向的code值，如果config不为null，则此值表示下标)
+    public func changeDirection(code: Int = 0, param: [String: Any]?, device: [String: Any]?) -> String? {
+        var res: String? = nil
+        if self.isSupportSceneBiz(param: param, device: device), let pt = self.currentKmpProtocol, let bizParam = self.currentScene?.dto, let speedConfig = bizParam.speedInfo?["config"] as? String {
+            pt.updateDirection(directionOrIndex: KotlinInt(int: Int32(code)), config: speedConfig)
+            res = _getCurrentBytes()
+        }
+        return res
+    }
+    
+    /// 调整速度（(注意：档位时传index，非档位传值)
+    public func changeDirection(value: Int = 0, param: [String: Any]?, device: [String: Any]?) -> String? {
+        var res: String? = nil
+        if self.isSupportSceneBiz(param: param, device: device), let pt = self.currentKmpProtocol, let bizParam = self.currentScene?.dto, let speedConfig = bizParam.speedInfo?["config"] as? String {
+            pt.updateSpeed(speedOrIndex: KotlinInt(int: Int32(value)), config: speedConfig)
+            res = _getCurrentBytes()
+        }
+        return res
+    }
+    /**
+     * 获取色调颜色组
+     */
+    public func toneColorInfo(param: [String: Any]?, device: [String: Any]?) -> [KmpToneColorsVo]? {
+        if self.isSupportSceneBiz(param: param, device: device), let pt = self.currentKmpProtocol {
+            let cls = pt.getToneColors().compactMap { KmpToneColorsVo(h: Float($0.h), colors: $0.toneColorList.map { $0.toSwiftInt()})}
+            return cls
+        }
         return nil
     }
     
-    /// 解析场景方向UI
-    public func directionUIParam(sceneBizParam: [String: Any], device: [String: Any]) -> KmpDirectionVo? {
-        
-        
+    public func getSceneConfig(param: [String: Any]?, device: [String: Any]?) -> KmpConfigVo? {
+        if self.isSupportSceneBiz(param: param, device: device), let pt = self.currentKmpProtocol, let bizParam = self.currentScene?.dto, let speedConfig = bizParam.speedInfo?["config"] as? String {
+            let cls = pt.getSceneConfigInfo(config: speedConfig)
+            
+            var speedVo: KmpSpeedVo? = nil
+            var directionVo: KmpDirectionVo? = nil
+            
+            // 转换speedInfo
+            if let speedInfo = cls?.speedInfo {
+                let speedRange = speedInfo.speedRange?.toSwiftIntArray()
+                speedVo = KmpSpeedVo(speedType: Int(speedInfo.speedType), 
+                               defSpeed: Int(speedInfo.defSpeed), 
+                               speedRange: speedRange, 
+                               speedGearCount: Int(speedInfo.speedGearCount))
+            }
+            
+            // 转换directionInfo
+            if let directionInfo = cls?.directionInfo {
+                let supportDirections = directionInfo.supportDirections?.toSwiftIntArray()
+                directionVo = KmpDirectionVo(defIndex: Int(directionInfo.defIndex), 
+                                           supportDirections: supportDirections)
+            }
+            
+            let configVo = KmpConfigVo()
+            configVo.speedInfo = speedVo
+            configVo.directionInfo = directionVo
+            
+            return configVo
+        }
         return nil
     }
-    
-    
-    
     
 }
 
--- GHKmpShare/Classes/Biz/KMPSharedModel.swift
+++ GHKmpShare/Classes/Biz/KMPSharedModel.swift
@@ -7,15 +7,157 @@
 
 import Foundation
 import HandyJSON
+import Shared
 
-public class KmpDirectionVo: HandyJSON {
-    
-    
-    public required init() { }
+@objcMembers
+public class KmpToneColorsVo:NSObject, HandyJSON {
+    var h: Float = 0.0
+    var colors: [Int] = []
+    public required override init() { }
+    public convenience init(h: Float, colors: [Int]) {
+        self.init()
+        self.h = h
+        self.colors = colors
+    }
 }
 
-public class KmpSpeedVo: HandyJSON {
+@objcMembers
+public class KmpConfigVo:NSObject, HandyJSON {
+    var speedInfo: KmpSpeedVo?
+    var directionInfo: KmpDirectionVo?
+    public required override init() { }
+}
+
+@objcMembers
+public class KmpSpeedVo:NSObject, HandyJSON {
+    var speedType: Int = 0
+    var defSpeed: Int = 0
+    var speedRange: [Int]? = nil
+    var speedGearCount: Int = 0
     
+    public required override init() { }
     
-    public required init() { }
+    convenience init(speedType: Int, defSpeed: Int, speedRange: [Int]?, speedGearCount: Int) {
+        self.init()
+        self.speedType = speedType
+        self.defSpeed = defSpeed
+        self.speedRange = speedRange
+        self.speedGearCount = speedGearCount
+    }
+}
+
+@objcMembers
+public class KmpDirectionVo:NSObject, HandyJSON {
+    var defIndex: Int = 0
+    var supportDirections: [Int]? = nil
+    
+    public required override init() { }
+    
+    convenience init(defIndex: Int, supportDirections: [Int]?) {
+        self.init()
+        self.defIndex = defIndex
+        self.supportDirections = supportDirections
+    }
+}
+
+
+
+@objcMembers
+public class KmpBizDto: NSObject, HandyJSON {
+    
+    public var base64String: String = ""
+    public var scenesType: Int = -1
+    public var speedInfo: [String: Any]?
+    
+    public required override init() { }
+}
+
+@objcMembers
+public class KmpDeviceDto: NSObject, HandyJSON {
+    
+    public var sku = ""
+    public var deviceID: String = ""
+    public var goodsType = 0
+    public var pactType = 0
+    public var pactCode = 0
+    public var versionHard = ""
+    public var versionSoft = ""
+    public var wifiHardVersion = ""
+    public var wifiSoftVersion = ""
+    public var deviceName: String = ""
+    public var deviceExt: [String : Any]?
+    
+    public required override init() { }
+}
+
+extension Data {
+    
+    init?(base64String: String) {
+        var string = base64String.replacingOccurrences(of: "\\", with: "")
+        let sub = string.count % 4
+        switch sub {
+        case 1, 2:
+            string = string.appending("==")
+        case 3:
+            string = string.appending("=")
+        default:
+            break
+        }
+        self.init(base64Encoded: string, options: .ignoreUnknownCharacters)
+    }
+    
+    func toKotlinByteArray() -> KotlinByteArray {
+        let kotlinByteArray = KotlinByteArray(size: Int32(self.count))
+        self.withUnsafeBytes { (rawBufferPointer: UnsafeRawBufferPointer) in
+            let bufferPointer = rawBufferPointer.bindMemory(to: UInt8.self)
+            for (index, byte) in bufferPointer.enumerated() {
+                kotlinByteArray.set(index: Int32(index), value: Int8(truncating: KotlinByte(value: Int8(bitPattern: byte))))
+            }
+        }
+        return kotlinByteArray
+    }
+}
+
+// Data 转 [SharedByte]
+extension Data {
+    func toSharedByteArray() -> [KotlinByte] {
+        return self.map { KotlinByte(value: Int8(bitPattern: $0)) }
+    }
+}
+
+// [SharedByte] 转 Data
+extension Array where Element == KotlinByte {
+    func toData() -> Data {
+        let bytes = self.map { UInt8(bitPattern: $0.int8Value) }
+        return Data(bytes)
+    }
+}
+
+extension KotlinInt {
+    /// 转换为 Swift Int
+    func toSwiftInt() -> Int {
+        return self.intValue
+    }
+    
+    /// 从 Swift Int 创建 KotlinInt
+    convenience init(swiftInt: Int) {
+        self.init(int: Int32(swiftInt))
+    }
+}
+
+extension Int {
+    /// 转换为 KotlinInt
+    func toKotlinInt() -> KotlinInt {
+        return KotlinInt(int: Int32(self))
+    }
+}
+
+extension KotlinIntArray {
+    func toSwiftIntArray() -> [Int] {
+        var result: [Int] = []
+        for i in 0..<self.size {
+            result.append(Int(self.get(index: i)))
+        }
+        return result
+    }
 }
